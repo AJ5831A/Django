6209cb500ccbf2d8db71b8b64665ee09f9d687e9
AJ5831A <jakhararyan27@gmail.com>
2025-03-13 23:14:00 +0530

    Hey this is new Begining

---
+++ hello.py
@@ -0,0 +1,6 @@
+sentence = input("Enter a sentence: ")
+print("Your sentence is:",sentence)
+w1 = input("Enter a word to get replaced: ")
+w2 = input("Enter a word to replace with: ")
+newSentence = sentence.replace(w1,w2)
+print("Your new sentence is:",newSentence)
