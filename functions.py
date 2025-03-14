44437b77683534b63297d9a14e8a065beb4ea506
AJ5831A <jakhararyan27@gmail.com>
2025-03-14 22:47:45 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+
+
+def gretings(name , age):
+    print("Welcome "+ name + ".\n You are " + str(age) + " years old")
+
+gretings(name , 56)
+
+num1 = int(input("Enter first numbr:"))
+num2 = int(input("Enter second numbr:"))
+
+def sum(num1 , num2):
+    return num1 + num2
+
+print(sum(num1 , num2))
